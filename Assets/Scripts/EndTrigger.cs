c3c71d5ada9132307151a8d2196e6970a2ca1603
Zesheng Chen <113056866+wingmancover@users.noreply.github.com>
2026-03-26 03:14:36 -0400

    EndTrigger added; Simple gameflow prototype is done

---
+++ Assets/Scripts/EndTrigger.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class EndTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Level Complete!");
+        }
+    }
+}
